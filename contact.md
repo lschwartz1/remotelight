f60d9a132e09c744eed4a1404729c3026402f55f
lschwartz1 <lauras94@vt.edu>
2017-03-28 10:22:12 -0400

    Added contact page

---
+++ contact.md
@@ -0,0 +1,14 @@
+# Contact Us
+
+##Telephone 
+
+703-blah blah blah
+
+##Email 
+
+Use our [website](www.remotelighting) to email us!
+
+![Remote Lights](https://static.pexels.com/photos/41113/pexels-photo-41113.jpeg)
+
+
+
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,17 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "c-disclaimer.html";
+fl["1"]= "contact.html";
+fl["2"]= "how-to-program-bulbs-to-groups.html";
+fl["3"]= "how-to-turn-on-off-dim-lights.html";
+fl["4"]= "intro-considerations-planning-light-network.html";
+fl["5"]= "intro-low-power-networking.html";
+fl["6"]= "intro-max-number-bulbs.html";
+fl["7"]= "intro-network-lighting.html";
+fl["8"]= "intro-product.html";
+fl["9"]= "ref-remote-control-components.html";
+fl["10"]= "ref-specifications-led-bulb.html";
+fl["11"]= "ref-specifications-overview.html";
+fl["12"]= "ref-specifications-remote-control.html";
+fl["13"]= "ts-remotelight.html";
+var doStem = false;searchLoaded = true;
